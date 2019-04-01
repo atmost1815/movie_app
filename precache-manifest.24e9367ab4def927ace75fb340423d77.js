458d6e5590a4e6face5893b2fc3d6cb6f23b065d
atmost1815 <silee9019@gmail.com>
2019-04-01 20:55:48 +0900

    Updates

---
+++ precache-manifest.24e9367ab4def927ace75fb340423d77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ece10032a4f56dbc1ee",
+    "url": "/movie_app/static/css/main.5d1a6b1a.chunk.css"
+  },
+  {
+    "revision": "5ece10032a4f56dbc1ee",
+    "url": "/movie_app/static/js/main.f7c02326.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  },
+  {
+    "revision": "74286a96ace76faac2ea",
+    "url": "/movie_app/static/js/2.5425dfe7.chunk.js"
+  },
+  {
+    "revision": "0cfbf677210ac437b5de6c298d85f071",
+    "url": "/movie_app/index.html"
+  }
+];
