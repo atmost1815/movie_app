586b9174da6df565002ad0ed164bd52d663483db
atmost1815 <silee9019@gmail.com>
2019-04-01 19:48:21 +0900

    css까지 완료

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {
+  Component
+} from 'react';
 import './App.css';
+import Movie from './Movie';
 
 class App extends Component {
+  state = {
+  }
+
+  componentDidMount() {
+    this._getMovies()
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie) =>
+      <Movie
+        key={movie.id}
+        title={movie.title_english}
+        poster={movie.medium_cover_image}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+      />
+    )
+    return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(console.error)
+
+
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : 'Loading'}
       </div>
-    );
+    )
   }
 }
 
